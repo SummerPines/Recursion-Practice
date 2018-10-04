168bd75d7da2ce74d055d524a3845d2114ca67a6
moodmela <42076806+SummerPines@users.noreply.github.com>
2018-10-04 11:32:30 -0600

    Created Factorial method

--- Recursion Practice/MainWindow.xaml.cs
+++ Recursion Practice/MainWindow.xaml.cs
@@ -21,5 +21,33 @@ namespace Recursion_Practice {
         public MainWindow() {
             InitializeComponent();
         }
+
+        /// <summary>
+        /// button handler for factorial code
+        /// tests for invalid entries and calls recursive function
+        /// </summary>
+        private void btnFactorial_Click(object sender, RoutedEventArgs e) {
+            if (!int.TryParse(txtFactorialInput.Text, out int factNum))
+                MessageBox.Show("Please enter an integer");
+            else if (factNum < 0)
+                MessageBox.Show("please enter a positive integer");
+
+            else
+                lblFactorial.Content = factorial(factNum).ToString();
+        }
+
+        /// <summary>
+        /// recursive factorial method
+        /// </summary>
+        /// <param name="num">the number to compute from</param>
+        /// <returns>the factorial (n*(n-1)*(n-2)...)</returns>
+        private int factorial(int num) {
+
+            //base case: have added all numbers or 0 was entered
+            if (num < 1 || num==0)
+                return 1;
+
+            return num * factorial(num - 1);
+        }
     }
 }
