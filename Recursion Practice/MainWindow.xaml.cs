dd9ea36f5233757cfc04ade5781711705d1f11cb
moodmela <42076806+SummerPines@users.noreply.github.com>
2018-10-04 12:34:44 -0600

    Added reverse array method

--- Recursion Practice/MainWindow.xaml.cs
+++ Recursion Practice/MainWindow.xaml.cs
@@ -22,6 +22,48 @@ namespace Recursion_Practice {
             InitializeComponent();
         }
 
+        //Class-level variables
+        private int[] arrayToReverse;
+
+
+        /// <summary>
+        /// Create Arrays and structures needed for methods when window is loaded
+        /// </summary>
+        private void wdwLoaded(object sender, RoutedEventArgs e) {
+            createReverseArray();
+        }
+
+
+        /// <summary>
+        /// Display a 1D array in a provided textbox
+        /// </summary>
+        /// <param name="array">a 1D array</param>
+        /// <param name="textbox">the textbox to display in</param>
+        private void dispayArray(int[] array, TextBox textbox) {
+
+            textbox.Text = "";
+            for (int i = 0; i < array.Length; i++ ) {
+                textbox.Text += array[i] + " ";
+            }
+        }
+
+        /// <summary>
+        /// Display a 2D array in a provided textbox
+        /// </summary>
+        /// <param name="array">a 2D array</param>
+        /// <param name="textbox">the textbox to display in</param>
+        private void dispayArray(int[,] array, TextBlock textblock) {
+
+            textblock.Text = "";
+            for (int r = 0; r < array.GetLength(0); r++) {
+                for (int c = 0; c < array.GetLength(1); c++) {
+                    textblock.Text += array[r, c].ToString().PadLeft(3);
+                }
+                textblock.Text += "\r\n";
+            }
+        }
+
+        #region Factorial
         /// <summary>
         /// button handler for factorial code
         /// tests for invalid entries and calls recursive function
@@ -49,5 +91,58 @@ namespace Recursion_Practice {
 
             return num * factorial(num - 1);
         }
+        #endregion Factorial
+
+        #region Reverse Array
+        /// <summary>
+        /// button handler to reverse an array
+        /// </summary>
+        private void btnReverseArray_Click(object sender, RoutedEventArgs e) {
+            reverseArray(arrayToReverse, 0);
+            dispayArray(arrayToReverse, txtReversedArray);
+        }
+
+        /// <summary>
+        /// reverseArray - a generic method
+        /// </summary>
+        /// <typeparam name="T">the type of variables in the array</typeparam>
+        /// <param name="array">the array to reverse</param>
+        /// <param name="index">the index to begin reversing at</param>
+        private void reverseArray<T> (T[] array, int index) {
+
+            //base case: half of the array has been flipped
+            if (index >= array.Length / 2)
+                return;
+
+            //flip elements at index
+            T temp = array[index];
+            array[index] = array[array.Length - 1 - index];
+            array[array.Length - 1 - index] = temp;
+
+            //continue with remainder of array
+            reverseArray(array, index + 1);
+        }
+
+        /// <summary>
+        /// button handler if radio button for reversed array size is changed
+        /// </summary>
+        private void rbCheckedChanged(object sender, RoutedEventArgs e) {
+            createReverseArray();
+        }
+
+        /// <summary>
+        ///create a sorted array of sequential numbers needed for the reverse array section
+        /// </summary>
+        private void createReverseArray() {
+
+            //size is determined by the radio buttons
+            int size = rbReverse19.IsChecked == true ? 19 : 20;
+            arrayToReverse = new int[size];
+            for (int i = 0; i < arrayToReverse.Length; i++)
+                arrayToReverse[i] = i;
+            dispayArray(arrayToReverse, txtInitialArray);
+            txtReversedArray.Text = "";
+        }
+        #endregion Reverse Array
     }
 }
